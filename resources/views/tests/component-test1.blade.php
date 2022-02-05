570541cad30a5f3c6953c1fa48e4a7a79e4380a2
yusuke804 <14au064f@al.rikkyo.ac.jp>
2022-02-05 21:14:03 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -6,5 +6,6 @@
 
     <x-tests.card title="タイトル1" content="本文1" :message="$message" />
     <x-tests.card title="タイトル2"  />
+    <x-tests.card title="CSSを変更したい" class="bg-red-300" />
 </x-tests.app>
 
