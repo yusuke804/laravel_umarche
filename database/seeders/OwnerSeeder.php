7b2a90202e4e5b39e2f9860bb069087ec261852b
yusuke804 <14au064f@al.rikkyo.ac.jp>
2022-02-07 22:23:41 +0900

    Carbon

---
+++ database/seeders/OwnerSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class OwnerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('owners')->insert([
+            [
+                'name' => 'test',
+                'email' => 'test@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2022/02/07 11:11:11',
+            ],
+            [
+                'name' => 'test2',
+                'email' => 'test2@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2022/02/07 11:11:11',
+            ],
+            [
+                'name' => 'test3',
+                'email' => 'test3@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2022/02/07 11:11:11',
+            ]
+
+        ]);
+
+    }
+}
