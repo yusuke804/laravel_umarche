2415d87f2118e30a253b06cfe5b78d958c9478df
yusuke804 <14au064f@al.rikkyo.ac.jp>
2022-02-11 16:46:52 +0900

    middleware, abort

---
+++ app/Http/Controllers/Owner/ShopController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Owner;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Shop;
+
+class ShopController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+
+            $id = $request->route()->parameter('shop');
+
+            if(!is_null($id)){
+
+                $shopsOwnerID = Shop::findOrFail($id)->owner->id;
+                $shopId = (int)$shopsOwnerID;
+                $ownerId = Auth::id();
+
+                if($shopId !== $ownerId){
+                    abort(404);
+                }
+
+            }
+            return $next($request);
+        });
+    }
+
+    public function index()
+    {
+        $ownerId = Auth::id();
+        $shops = Shop::where('owner_id', '=', $ownerId)->get();
+
+        return view('owner.shops.index', compact('shops'));
+    }
+
+
+    public function edit($id)
+    {
+        dd(Shop::findOrFail($id));
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+
+}
