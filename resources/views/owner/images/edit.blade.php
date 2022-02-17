cfddd58bac8df7f33447da64ed54d435e65f0808
yusuke804 <14au064f@al.rikkyo.ac.jp>
2022-02-17 12:31:57 +0900

    image delete

--- resources/views/owner/images/edit.blade.php
+++ resources/views/owner/images/edit.blade.php
@@ -33,8 +33,23 @@
                           <button type="submit" class="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">更新する</button>
                       </div>
                   </form>
+                  <form id="delete_{{ $image->id }}" method="POST" action="{{ route('owner.images.destroy', ['image' => $image->id]) }}">
+                    @csrf
+                    @method('DELETE')
+                    <div class="p-2 w-full flex justify-around mt-32">
+                        <a href="#" data-id="{{ $image->id }}" onclick="deletePost(this)" class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded ">削除する</a>
+                    </div>
+                </form>
                 </div>
             </div>
         </div>
     </div>
+    <script>
+        function deletePost(e) {
+            'use strict';
+            if (confirm('本当に削除してもいいですか?')) {
+            document.getElementById('delete_' + e.dataset.id).submit();
+            }
+        }
+    </script>
   </x-app-layout>
